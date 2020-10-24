48bae61fe69d5180839c83b51021dd5dd16335fb
matiasbiderman <70668471+matiasbiderman@users.noreply.github.com>
2020-10-24 13:42:07 -0300

    armando solucion con dos errores

---
+++ AccesoDatosClientes/AccesoDatos.Entidades/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.Entidades
+{
+    [DataContract]
+    public class Cliente
+    {
+        private string _nombre;
+        private string _apellido;
+        private string _direccion;
+        private int _id;
+
+        [DataMember]
+        public int Id { get => _id; set => _id = value; }
+
+        [DataMember]
+        public string Nombre { get => _nombre; set => _nombre = value; }
+
+        [DataMember(Name = "apellido")]
+        public string Ape { get => _apellido; set => _apellido = value; }
+
+        public string Direccion { get => _direccion; set => _direccion = value; }
+
+        public override string ToString()
+        {
+            return string.Format("Cliente {0}, {1}", this._apellido, this._nombre);
+        }
+    }
+}
---
+++ AccesoDatosClientes/AccesoDatos.Negocio/ClienteServicio.cs
@@ -0,0 +1,66 @@
+﻿using AccesoDatos.Datos;
+using AccesoDatos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.Negocio
+{
+    class ClienteServicio
+    {
+        private ClienteMapper mapper;
+        public ClienteServicio()
+        {
+            mapper = new ClienteMapper();
+        }
+
+        public List<Cliente> TraerClientes()
+        {
+            List<Cliente> result = mapper.TraerTodos();
+            return result;
+        }
+
+        public Cliente TraerClientesPorId(int id)
+        {
+            List<Cliente> result = mapper.TraerTodos();
+            Cliente cli = null;
+            foreach (Cliente c in result)
+            {
+                if (c.Id == id)
+                    cli = c;
+            }
+            return cli;
+
+        }
+
+        public int InsertarCliente(string nombre, string apellido, string direccion)
+        {
+            // antes validar con el get si existe ese dni/apellido-nombre
+            List<Cliente> result = mapper.TraerTodos();
+
+            Cliente cliente = new Cliente();
+            cliente.Ape = apellido;
+            cliente.Nombre = nombre;
+            cliente.Direccion = direccion;
+
+            foreach (var item in result)
+            {
+                if (item.ToString() == cliente.ToString())
+                    throw new Exception("El cliente ya existe");
+            }
+
+            // ya pasamos las validaciones, persistimos el cliente
+            TransactionResult resultante = mapper.Insert(cliente);
+
+            if (resultante.IsOk)
+                return resultante.Id;
+            //return TraerClientesPorId(resultante.Id);
+            else
+
+                throw new Exception("Hubo un error en la petición al servidor. Detalle: " + resultante.Error);
+
+        }
+    }
+}

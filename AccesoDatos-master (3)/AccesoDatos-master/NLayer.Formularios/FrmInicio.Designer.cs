e131d5f04dd385de561a5ce44e671c23d99ba47e
matiasbiderman <70668471+matiasbiderman@users.noreply.github.com>
2020-10-31 22:20:40 -0300

    -

--- AccesoDatos-master (3)/AccesoDatos-master/NLayer.Formularios/FrmInicio.Designer.cs
+++ AccesoDatos-master (3)/AccesoDatos-master/NLayer.Formularios/FrmInicio.Designer.cs
@@ -31,12 +31,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(57, 21);
+            this.button1.Location = new System.Drawing.Point(189, 37);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(213, 51);
             this.button1.TabIndex = 0;
@@ -46,7 +45,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(57, 195);
+            this.button2.Location = new System.Drawing.Point(189, 252);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(213, 51);
             this.button2.TabIndex = 1;
@@ -56,7 +55,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(57, 108);
+            this.button3.Location = new System.Drawing.Point(189, 161);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(213, 37);
             this.button3.TabIndex = 2;
@@ -64,21 +63,11 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(361, 195);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(213, 51);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "MODIFICAR SALDO";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // FrmInicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(607, 379);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -93,6 +82,5 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button5;
     }
 }

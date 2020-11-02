8e96df5170a8402185b93a5a19788cd8ed720a50
matiasbiderman <70668471+matiasbiderman@users.noreply.github.com>
2020-11-02 11:01:02 -0300

    con posibilidad de sacar pdf harcodeado

--- AccesoDatos-master (3)/AccesoDatos-master/NLayer.Formularios/FrmListaClientes.Designer.cs
+++ AccesoDatos-master (3)/AccesoDatos-master/NLayer.Formularios/FrmListaClientes.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -61,11 +63,30 @@
             this.listBox1.Size = new System.Drawing.Size(214, 225);
             this.listBox1.TabIndex = 2;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(635, 161);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 80);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "SACAR REPORTE DE CLIENTE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(436, 383);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
             // FrmListaClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -73,6 +94,7 @@
             this.Text = "FrmListaClientes";
             this.Load += new System.EventHandler(this.FrmListaClientes_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +103,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- AccesoDatos-master (3)/AccesoDatos-master/NLayer.Formularios/FrmListaClientes.cs
+++ AccesoDatos-master (3)/AccesoDatos-master/NLayer.Formularios/FrmListaClientes.cs
@@ -1,10 +1,13 @@
-﻿using NLayer.Entidades;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using NLayer.Entidades;
 using NLayer.Negocio;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,7 +42,92 @@ namespace NLayer.Formularios
 
         private void FrmListaClientes_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            /*
+             OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK) { }
+            //textBox1.Text = openFileDialog1.file();*/
+             
+    /*
+            Document doc = new Document(PageSize.LETTER);
+            // Indicamos donde vamos a guardar el documento
+            PdfWriter writer = PdfWriter.GetInstance(doc,
+                                        new FileStream(@"C:\Users\Mati\Documents\prueba.pdf", FileMode.Create));
+
+            // Le colocamos el título y el autor
+            // **Nota: Esto no será visible en el documento
+            doc.AddTitle("Mi primer PDF");
+            doc.AddCreator("Roberto Torres");
+
+            // Abrimos el archivo
+            doc.Open();
+
+            iTextSharp.text.Font _standardFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+
+            // Escribimos el encabezamiento en el documento
+            doc.Add(new Paragraph("Mi primer documento PDF"));
+            doc.Add(Chunk.NEWLINE);
+
+            // Creamos una tabla que contendrá el nombre, apellido y país
+            // de nuestros visitante.
+            PdfPTable tblPrueba = new PdfPTable(2);
+            tblPrueba.WidthPercentage = 100;
+
+            // Configuramos el título de las columnas de la tabla
+            PdfPCell clNombre = new PdfPCell(new Phrase("Nombre", _standardFont));
+            clNombre.BorderWidth = 0;
+            clNombre.BorderWidthBottom = 0.75f;
+            PdfPCell[] celdanombres = new PdfPCell[4];
+
+            // List<PdfPCell> celdanombres = new List<PdfPCell>();
+            PdfPCell clApellido = new PdfPCell(new Phrase("Apellido", _standardFont));
+            clApellido.BorderWidth = 0;
+            clApellido.BorderWidthBottom = 0.75f;
+            PdfPCell[] celdapellidos = new PdfPCell[4];//= new List<PdfPCell>();
+
+
+
+            // Añadimos las celdas a la tabla
+            tblPrueba.AddCell(clNombre);
+            tblPrueba.AddCell(clApellido);
+            // tblPrueba.AddCell(clPais);
+
+
+            // Llenamos la tabla con información
+            //List <Cliente> lst = _clienteServicio.TraerClientes();
+            string[] lst = { "hola", "chau", "asasasasasa", "dfdsfdsfsdfds" };
+
+
+            for (int i = 0; i < lst.Length; i++)
+            {
+                clNombre = new PdfPCell(new Phrase(lst[i], _standardFont));
+                clNombre.BorderWidth = 0;
+                celdanombres[i] = clNombre;
+                clApellido = new PdfPCell(new Phrase(lst[i], _standardFont));
+                clApellido.BorderWidth = 0;
+                celdapellidos[i] = clApellido;
+                tblPrueba.AddCell(celdanombres[i]);
+                tblPrueba.AddCell(celdapellidos[i]);
+
+            }
+
+            doc.Add(tblPrueba);
+            // Añadimos las celdas a la tabla
+            doc.Close();
+            writer.Close();
+
+            MessageBox.Show("El reporte fue exportado correctamente. Lo podra encontrar en Documentos como prueba.pdf");
+            */
         }
     }
 }
